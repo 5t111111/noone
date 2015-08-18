95d22a82cbb24cbf6f0dfa661526edd643ae6978
Hirofumi Wakasugi <baenej@gmail.com>
2015-08-18 12:17:27 +0900

    Implement core features

--- lib/noone.rb
+++ lib/noone.rb
@@ -1,5 +1,32 @@
 require "noone/version"
 
 module Noone
-  # Your code goes here...
+  KEYBOARD_TABLE = {
+                    "a" => "C",
+                    "w" => "C#",
+                    "s" => "D",
+                    "e" => "D#",
+                    "d" => "E",
+                    "f" => "F",
+                    "t" => "F#",
+                    "g" => "G",
+                    "y" => "G#",
+                    "h" => "A",
+                    "u" => "A#",
+                    "j" => "B"
+                   }
+  NOTE_TABLE = {
+                "C"  => 36,
+                "C#" => 37,
+                "D"  => 38,
+                "D#" => 39,
+                "E"  => 40,
+                "F"  => 41,
+                "F#" => 42,
+                "G"  => 43,
+                "G#" => 44,
+                "A"  => 45,
+                "A#" => 46,
+                "B"  => 47
+               }
 end
